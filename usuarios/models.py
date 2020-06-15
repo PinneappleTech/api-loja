14b9945e60134a6a8e907abc31cbc2dca333cc71
christian-oliveira <christianoliveirati@gmail.com>
2020-06-14 23:43:09 -0300

    criando app de usuarios

---
+++ usuarios/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    SEXO = (
+        (1, 'FEMININO'),
+        (2, 'MASCULINO'),
+    )
+
+    VENDEDOR = 1
+    CAIXA = 2
+    SUPERVISOR = 3
+    ADMIN = 4
+
+    TIPOS_USUARIOS = (
+        (VENDEDOR, 'VENDEDOR'),
+        (CAIXA, 'CAIXA'),
+        (SUPERVISOR, 'SUPERVISOR'),
+        (ADMIN, 'ADMINISTRADOR'),
+    )
+
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, default=True)
+    tipo_usuario = models.PositiveSmallIntegerField(choices=TIPOS_USUARIOS, default=VENDEDOR)
+    sexo = models.PositiveSmallIntegerField(choices=SEXO)
