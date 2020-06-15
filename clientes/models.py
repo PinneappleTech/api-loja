938d3ee1ad62d03dd955bd8e1875c73937458366
christian-oliveira <christianoliveirati@gmail.com>
2020-06-14 23:42:48 -0300

    criando modelo de Endereco e add no Admin

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Base(models.Model):
+    criado_em     = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+    ativo       = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Endereco(Base):
+    endereco  = models.CharField(max_length=255)
+    bairro    = models.CharField(max_length=100)
+    cep       = models.CharField(max_length=8)
+    numero    = models.SmallIntegerField(blank=True, null=True)
+    uf        = models.CharField(max_length=2)
+    cidade    = models.CharField(max_length=100)
+    ponto_ref = models.CharField(max_length=150, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Endereço"
+        verbose_name_plural = "Endereços"
+    
+    def __str__(self):
+        return self.endereco
+
+
+class Cliente(Base):
+    #FEMININO, MASCULINO
+    SEXO = (
+        (1, 'FEMININO'),
+        (2, 'MASCULINO'),
+    )
+
+    #CASADO, DIVORCIADO, SOLTEIRO, VIUVO
+    ESTADO_CIVIL = (
+        (1, 'CASADO(A)'),
+        (2, 'DIVORCIADO(A)'),
+        (3, 'SEPARADO(A)'),
+        (4, 'SOLTEIRO(A)'),
+        (5, 'VIUVO(A)'),
+    )
+
+    nome           = models.CharField(max_length=100)
+    cpf            = models.CharField(max_length=11)
+    rg             = models.CharField(max_length=15, blank=True, null=True)
+    sexo           = models.IntegerField(choices=SEXO)
+    data_nasc      = models.DateField()
+    fone           = models.CharField(max_length=11)
+    email          = models.EmailField()
+    estado_civil   = models.IntegerField(choices=ESTADO_CIVIL)
+    conjuge        = models.CharField(max_length=100, blank=True, null=True)
+    apelido        = models.CharField(max_length=50, blank=True, null=True)
+    filiacao       = models.CharField(max_length=100)
+    credito        = models.DecimalField(max_digits=8, decimal_places=2)
+    obs            = models.TextField(blank=True, null=True)
+    criado_por     = models.ForeignKey(User, related_name='clientes', on_delete=models.SET_NULL, null=True)
+    endereco       = models.ForeignKey(Endereco, related_name="enderecos", on_delete=models.SET_NULL, null=True)
+    end_entrega    = models.ForeignKey(Endereco, related_name="enderecos_entregas", on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
