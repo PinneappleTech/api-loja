e8cc8bd17dd15ce4b9ef59c8341ca544c8028c5e
christian-oliveira <christianoliveirati@gmail.com>
2020-07-03 23:04:24 -0300

    corrigindo algumas configurações

--- loja/settings.py
+++ loja/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'loja.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': config('DB_ENGINE'),
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
