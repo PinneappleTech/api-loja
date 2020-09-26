cd69edde60067ac95248416d35b8f5afdf3b7fe9
christian-oliveira <christianoliveirati@gmail.com>
2020-09-26 11:36:36 -0300

    :construction: corrigindo urls

--- loja/views.py
+++ loja/views.py
@@ -10,16 +10,20 @@ class CustomAuthToken(ObtainAuthToken):
         serializer = self.serializer_class(data=request.data, context={'request': request})
 
         if not serializer.is_valid():
-            return Response({ "Error":"Usuário ou senha incorretos." }, status=status.HTTP_400_BAD_REQUEST)
+            return Response({ "message": "Usuário ou senha incorretos." }, status=status.HTTP_400_BAD_REQUEST)
 
         user = serializer.validated_data['user']
 
         if user.perfil.tipo_usuario == 1:
-            return Response("Login do usuário não permitido.", status=status.HTTP_401_UNAUTHORIZED)
+            return Response({ "message": "Login do usuário não permitido." }, status=status.HTTP_401_UNAUTHORIZED)
 
         token, created = Token.objects.get_or_create(user=user)
-
+        print(user.perfil.tipo_usuario)
         return Response({
-            'user_id': user.pk,
-            'token': token.key
+            'user': {
+                'id': user.id,
+                'nome': user.username,
+                'tipo_usuario': user.perfil.tipo_usuario
+            },
+            'token': token.key,
         })
